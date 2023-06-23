532c69029e75774f40c429e99d9c120bda781c3f
JeffRisberg <j.s.risberg@gmail.com>
2023-06-22 23:03:59 -0700

    began to fill in the first page.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        const title = React.createElement("h1", null, "Hello world!");
+        const link = React.createElement("a", { href: "//reactjs.org" }, "Read more");
+        const group = React.createElement("div", null, title, link);
+        const domElement = document.getElementById("root"); ReactDOM.createRoot(domElement).render(group);
       </header>
     </div>
   );
