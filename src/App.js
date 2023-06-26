1a4c0ebe5970da1575c0e0402888bc478261631d
JeffRisberg <j.s.risberg@gmail.com>
2023-06-25 18:34:47 -0700

    updated example from ch03

--- src/App.js
+++ src/App.js
@@ -2,32 +2,22 @@ import React, {Component, Fragment} from "react";
 
 class Link extends Component {
     render() {
-        return React.createElement(
-            "p",
-            null, React.createElement(
-                "a",
-                {href: this.props.url},
-                `Read more about ${this.props.framework}`
-            ));
+        return (
+            <p>
+            <a href={this.props.url}>{this.props.children}</a>
+            </p>
+        )
     }
 }
 
 class App extends Component {
     render() {
-        const link1 = React.createElement(Link, {
-            framework: "React",
-            url: "//reactjs.org",
-        });
-        const link2 = React.createElement(Link, {
-            framework: "Vue",
-            url: "//vuejs.org",
-        });
-        const link3 = React.createElement(Link, {
-            framework: "Angular",
-            url: "//angular.io",
-        });
-        return React.createElement(Fragment, null, link1, link2, link3);
-    }
+        return (
+            <Fragment>
+                <Link url="//reactjs.org">
+                    <strong>React</strong></Link>
+                <Link url="//vuejs.org">Vue</Link>
+                <Link url="//angular.io">Angular</Link> </Fragment>
+        ); }
 }
-
 export default App;
