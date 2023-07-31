cba9ee160b631c2a19907a5a726c0b1ed8381449
JeffRisberg <j.s.risberg@gmail.com>
2023-07-30 17:28:36 -0700

    began to rebuild Timer example

--- src/Timer.js
+++ src/Timer.js
@@ -1,14 +1,44 @@
-import {React, Component} from 'react';
+import Button from "./Button";
+import TimeDisplay from "./TimeDisplay";
+import useTimer from "./useTimer";
 
-class Timer extends Component {
-  render() {
-    if (this.props.timeLeft === 0) {
-      document.getElementById('end-of-time').play()
-    }
-    if (this.props.timeLeft === null || this.props.timeLeft === 0)
-      return <div/>
-    return <h1>Time left: {this.props.timeLeft}</h1>
-  }
+function Timer({ startTime, id, onDelete }) {
+  const {
+    state: { remaining, isRunning, isCompleted },
+    dispatch,
+  } = useTimer(startTime);
+
+  const timerClass = [
+    "timer",
+    isCompleted ? "timer-ringing" : "",
+    isRunning ? "timer-ticking" : "",
+  ].join(" ");
+
+  return (
+    <section className={timerClass}>
+      <TimeDisplay time={remaining} />
+      {isRunning ? (
+        <Button
+          icon="pause"
+          label="Pause"
+          onClick={() => dispatch({ type: "PAUSE" })}
+        />
+      ) : (
+        <Button
+          icon="play"
+          label="Play"
+          onClick={() => dispatch({ type: "PLAY" })}
+          disabled={isCompleted}
+        />
+      )}
+      <Button
+        icon="restart"
+        label="Restart"
+        onClick={() => dispatch({ type: "RESTART" })}
+      />
+      <Button icon="trash" label="Delete" onClick={() => onDelete(id)} />
+    </section>
+  );
 }
 
 export default Timer;
